22c9060314487f1b026c48e50c8ae5d7bd79acc7
JaneLiu <342707049@qq.com0>
2021-12-29 10:44:01 +0800

    add new line for function

--- calculator.py
+++ calculator.py
@@ -8,6 +8,7 @@ num2=input("Enter the second number:")
 
 sum=float(num1)+float(num2)
 
-print("number {0} and {1} result is: {2}".format(num1, num2, sum))
+print("number {0} and {1} result is: {2}".format(num1, num2, sum)
 
+print ('this is only a test line!!!')
 
