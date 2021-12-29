b4514aa7e498ab65ab71be38dea547bbe4f2fb86
JaneLiu <342707049@qq.com0>
2021-12-29 09:56:57 +0800

    add new functions for issue53

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#Enter your numbers
+
+num1=input("Enter the first number:")
+num2=input("Enter the second number:")
+
+sum=float(num1)+float(num2)
+
+print("number {0} and {1} result is: {2}".format(num1, num2, sum))
+
+
