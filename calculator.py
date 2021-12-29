1b13b6643b178d2a596f2592fc3badfb425f485c
JaneLiu <342707049@qq.com0>
2021-12-29 10:57:35 +0800

    modify sum to sum1

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@
 num1=input("Enter the first number:")
 num2=input("Enter the second number:")
 
-sum=float(num1)+float(num2)
+sum1=float(num1)+float(num2)
 
 print("number {0} and {1} result is: {2}".format(num1, num2, sum)
 
